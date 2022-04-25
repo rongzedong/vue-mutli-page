fda8eff1d02bf561d697ef386e40d8e4b893198a
rongzedong <rongzedong@users.noreply.github.com>
2022-04-25 15:57:49 +0800

    Update router.js

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -1,3 +1,4 @@
+// 这个作为插件保存在这里，也可以单独建立router目录，存在那边，当然要修改对应的引用文件里的内容
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import viewHome from '../views/viewHome.vue'
@@ -14,10 +15,12 @@ const routes = [
   // },
   {
     path: '/',
+    title: 'title',
     component: viewHome,
   },
   {
     path: '/home',
+    title: 'home',
     component: viewHome,
     children: [
       // 当 /home/login 匹配成功，Login会被渲染在Home的<router-view>中
@@ -28,10 +31,14 @@ const routes = [
   },
   {
     path: '/login',
+    meta: {
+    title: 'login',
+    },
     component: viewLogin
   },
   {
     path: '/register',
+    title: 'reg',
     component: viewRegister
   },
 ]
@@ -40,8 +47,27 @@ const router = new VueRouter({
   routes
 })
 
+// document.title = getPageTitle(to.meta.title)
+//  export default function getPageTitle(pageTitle) {
+//   if (pageTitle) {
+//     return `${pageTitle} - ${title}`
+//   }
+//   return `${title}`
+// }
+
+import defaultSettings from '@/settings'
+const title = defaultSettings.title || 'vue-mutli-page'
+
 // 添加导航守卫
 router.beforeEach((to, from, next) => {
+
+  console.log(to)
+  if (to.meta.title) {
+    document.title = `${to.meta.title} - ${title}`
+  }else{
+    document.title = `${title}`
+  }
+
   // 如果目标路由为登录或注册页面，直接放行
   if (to.path === '/' || to.path === '/login' || to.path === '/register') {
     return next()
