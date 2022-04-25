2be3caa9d5ffe6851f1cc4fa773915159d0558f8
rongzedong <rongzedong@users.noreply.github.com>
2022-04-25 19:28:12 +0800

    Add files via upload

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
+const defaultSettings = require('./src/settings.js')
+const name = defaultSettings.title || 'vue-mutli-page' // page title
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+   configureWebpack: {
+    // provide the app's title in webpack's name field, so that
+    // it can be accessed in index.html to inject the correct title.
+    name: name,},
 })
